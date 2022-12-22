7df8848fe56729e04529551782128c48a4bf3b81
vishwajeethaldar <onlyjeet3@gmail.com>
2022-12-23 01:19:42 +0530

    add

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-const express = require("express")
-
-const app = express()
+const express = require('express');
+const app = express();
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  const path = `/api/item`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-app.listen(process.env.port||8000, ()=>{
-  console.log("Hello");
+  res.sendFile('index.html');
 })
 
 
+app.listen(process.env.PORT || 3000);
+
+module.exports = app
