e0285b8801a6845c22175da4dd0f9c5007330b0c
vishwajeethaldar <onlyjeet3@gmail.com>
2022-12-23 01:05:55 +0530

    updated

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require("express")
 const app = express()
 app.use(express.static('public'))
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
+app.get('/', (req, res) => {
+  const path = `/api/item`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
